23d5474132f014a3953cfc8d465394882867fbf7
vynzm90 <vynzm@Victors-MacBook-Pro.local>
2015-01-19 22:14:55 -0700

    First version of ronda dev with parse notifications.

---
+++ Ronda/Ronda-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Ronda-Bridging-Header.h
+//  Ronda
+//
+//  Created by Victor Yanez on 1/19/15.
+//  Copyright (c) 2015 eCloud. All rights reserved.
+//
+
+#ifndef Ronda_Ronda_Bridging_Header_h
+#define Ronda_Ronda_Bridging_Header_h
+#endif
+#import <Parse/Parse.h>
+#import <ParseCrashReporting/ParseCrashReporting.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import "CrashHandler.h"
---
+++ Ronda/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ronda
+//
+//  Created by Victor Yanez on 1/19/15.
+//  Copyright (c) 2015 eCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func ret()->String?{
+        return nil;
+    }
+}
+
